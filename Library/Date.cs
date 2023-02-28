6bd9ee8099a83525ef4a3f4e79b903148c354bb5
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2023-02-28 12:05:02 +0200

    Create library and class Person

---
+++ Library/Date.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Date
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public Date()/*По замовчуванню*/
+        {
+            Year = 2023;
+            Month = 2;
+            Day = 3;
+        }
+        public Date(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public Date(Date date)/*Копіювання*/
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+        }
+    }
+
+}
---
+++ Library/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Library
+{
+    public class Person
+    {
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public Date Birthday { get; set; }
+
+        public Person(string firstName, string lastName, Date birthday)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = new Date(birthday);
+        }
+    }
+}
