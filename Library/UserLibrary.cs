d01f0b94d96f4ecffc1d674a35c0adbd820e7a95
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2023-03-01 14:28:10 +0200

    Create method ShowInfo to class UserLibrary

--- Library/UserLibrary.cs
+++ Library/UserLibrary.cs
@@ -8,29 +8,33 @@ namespace Library
 {
     public class UserLibrary:Person
     {
-        public string LibraryCardNumber { get; set; }
+        public int LibraryCardNumber { get; set; }
         public Date IssueDate { get; set; }
         public int MonthlyReadingFee { get; set; }
-        public UserLibrary(string firstName, string lastName, Date birthday, string libraryCardNumber, Date issueDate, int monthlyReadingFee)
+        public UserLibrary(string firstName, string lastName, Date birthday, int libraryCardNumber, Date issueDate, int monthlyReadingFee)
             : base (firstName, lastName, birthday)
         {
             LibraryCardNumber = libraryCardNumber;
             IssueDate = new Date(issueDate);
             MonthlyReadingFee = monthlyReadingFee;
         }
-        public UserLibrary(string firstName, string lastName, Date birthday, string libraryCardNumber, Date issueDate)
+        public UserLibrary(string firstName, string lastName, Date birthday, int libraryCardNumber, Date issueDate)
             : base(firstName, lastName, birthday)
         {
             LibraryCardNumber = libraryCardNumber;
             IssueDate = new Date(issueDate);
             MonthlyReadingFee = 0;
         }
-        public UserLibrary(string firstName, string lastName, Date birthday, string libraryCardNumber)
+        public UserLibrary(string firstName, string lastName, Date birthday, int libraryCardNumber)
             : base(firstName, lastName, birthday)
         {
             LibraryCardNumber = libraryCardNumber;
             IssueDate = new Date();
             MonthlyReadingFee = 0;
+        }
+        public override string ShowInfo()
+        {
+            return $"Ім'я - {FirstName}, Прізвище - {LastName}, Дата народження - {Birthday.GetInfo()}, Номер читацького квитка - {LibraryCardNumber}, Дата видачі - {IssueDate.GetInfo()}, Розмір щомісячного читацького внеска - {MonthlyReadingFee} грн.";
         }
 
     }
--- VirtualizationApp/Program.cs
+++ VirtualizationApp/Program.cs
@@ -9,6 +9,6 @@ Person person= new Person("Віталій", "Прухницький", new Date(3
 Person applicant = new Applicant("Яніна", "Стельмашенко", new Date(10, 10, 2001), new ZNO(150, 160, 140, 0), 9, "Озерненський ліцей");
 Person student = new Student("Яніна", "Стельмашенко", new Date(10, 10, 2001), 1, "15Мд-СОІнф", "Фізико-математичний", "Житомирський державний університет ім. І. Франка");
 Person lecturer = new Lecturer("Сергій", "Жуковський", new Date(10, 10, 1985), "Доцент, кандидат педагогічних наук", "Кафедра комп'ютерних наук та інформаційних технологій", "Житомирський державний університет ім. І. Франка");
+Person userLibrary = new UserLibrary("Яніна", "Стельмашенко", new Date(10, 10, 2001), 24, new Date(1, 3, 2023), 200);
 
-
-Console.WriteLine(student.ShowInfo());
+Console.WriteLine(userLibrary.ShowInfo());
