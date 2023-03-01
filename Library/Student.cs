dccc7559932bdefacf15843bf4e36b81fd9ed5c8
Vitaliy Prukhnitskiy <Vitaliy152222@gmail.com>
2023-03-01 11:41:41 +0200

    Create class Student and constructors

---
+++ Library/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Student : Person
+    {
+        public int Course { get; set; }
+        public string Groupe { get; set; }
+        public string Faculty { get; set; }
+        public string University { get; set; }
+
+        public Student(string firstName, string lastName, Date birthday, int course, string groupe, string faculty, string university)
+            :base(firstName, lastName, birthday)
+        { 
+            Course = course;
+            Groupe = groupe;
+            Faculty = faculty;
+            University = university;
+        }
+        public Student(string firstName, string lastName, Date birthday, int course, string groupe, string faculty)
+            : base(firstName, lastName, birthday)
+        {
+            Course = course;
+            Groupe = groupe;
+            Faculty = faculty;
+            University = "Житомирський державний університет ім. І. Франка";
+        }
+        public Student(string firstName, string lastName, Date birthday, string groupe, string faculty, string university)
+            : base(firstName, lastName, birthday)
+        {
+            Course = 1;
+            Groupe = groupe;
+            Faculty = faculty;
+            University = university;
+        }
+    }
+}
